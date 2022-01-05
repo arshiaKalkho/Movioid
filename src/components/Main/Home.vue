545aa90f19b73b8c1bf1e811bb1b1a989bbb28ed
Arshia Kalkhorani <arshiabdol@yahoo.com>
2022-01-04 16:56:05 -0800

    added base elements

---
+++ src/components/Main/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header></Header>
+  <div>home page</div>
+  <Footer></Footer>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<sytle>
+
+</sytle>
---
+++ src/components/TopPicks.vue/TopPicks.vue
@@ -0,0 +1,9 @@
+<template lang="">
+  <div></div>
+</template>
+<script>
+//Top picks is a place holder for search result before user
+//has searched for something
+export default {};
+</script>
+<style lang=""></style>
