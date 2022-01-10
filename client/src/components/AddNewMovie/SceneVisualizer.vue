fab4c572e19fba06d4b33e96a23b97a415db8131
Arshia Kalkhorani <arshiabdol@yahoo.com>
2022-01-09 20:20:57 -0800

    syncing

---
+++ client/src/components/AddNewMovie/SceneVisualizer.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <div class="scene-list-visual">
+        
+        {{sceneList[0]}}
+    </div>
+</template>
+<script>
+export default {
+    name:"ScneceVisualizer",
+    props:['sceneList']            
+        
+        
+    
+}
+</script>
+<style lang="css">
+    .scene-list-visual{
+        width: 2rem;
+        height: 2rem;
+        background-color: red;
+    }
+</style>
