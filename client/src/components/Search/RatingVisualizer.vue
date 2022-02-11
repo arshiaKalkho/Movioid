ab109dd6fe6b8316197d480c7f8834fae2db5fa3
arshia <arshiabdol@yahoo.com>
2022-02-10 19:55:17 -0500

    added RatingVisualizer component

---
+++ client/src/components/Search/RatingVisualizer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        rating:{{rating}}
+    </div>
+</template>
+<script>
+
+
+export default {
+  name:"movies",
+  props:{
+      rating:Number
+  },
+}
+
+</script>
+<style lang="css">
+
+</style>
